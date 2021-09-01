ca20c3cae6301f74b5af1242879ac2494c151919
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2021-09-02 00:02:19 +0900

    feat: Add client

---
+++ cancel/client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/bellwood4486/sample-go-grpc/cancel/helloworld"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:8080"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "Bob"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
